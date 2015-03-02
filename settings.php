3939418e86af75f0705b89742eef74d3933920d4
Emir <emiratesbp@gmail.com>
2015-03-02 15:44:51 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="477 East Butterfield Road, Suite 101"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Lombard, IL 60148";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6307241400;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="pop"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
